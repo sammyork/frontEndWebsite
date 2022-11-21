0f4b792803e14154df8264c87ec868fba97008bf
sammyork <106724801+sammyork@users.noreply.github.com>
2022-11-21 00:37:16 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,49 @@
+var defaultThreads = [
+    {
+        id: 1,
+        title: "Information",
+        author: "Anonymous",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+        ]
+    },
+    {
+        id: 2,
+        title: "Questions",
+        author: "Anonymous",
+        date: Date.now(),
+        content: "Thread content 2",
+        comments: [
+        ]
+    },
+    {
+        id: 3,
+        title: "Tutoring",
+        author: "Anonymous",
+        date: Date.now(),
+        content: "Thread content 3",
+        comments: [
+        ]
+    },
+    {
+        id: 4,
+        title: "Off Topic",
+        author: "Anonymous",
+        date: Date.now(),
+        content: "Thread content 4",
+        comments: [
+        ]
+    }
+]
+
+var threads = defaultThreads
+if (localStorage && localStorage.getItem('threads')) {
+    threads = JSON.parse(localStorage.getItem('threads'));
+} 
+else {
+    threads = defaultThreads;
+    localStorage.setItem('threads', JSON.stringify(defaultThreads));
+}
+
+//Samantha's page
